fbe63771d0befe688f112d678de3c0d0cdef9c89
Manahil Sabeeh <msabeeh2007@gmail.com>
2026-09-22 22:57:37 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_counts = {}
+        t_counts = {}
+
+        for letter in s:
+            s_counts[letter] = s_counts.get(letter, 0) + 1
+        
+        for letter in t:
+            t_counts[letter] = t_counts.get(letter, 0) + 1
+
+        sorted_s = dict(sorted(s_counts.items()))
+        sorted_t = dict(sorted(t_counts.items()))
+
+        if sorted_s == sorted_t:
+            return True
+        else:
+            return False
